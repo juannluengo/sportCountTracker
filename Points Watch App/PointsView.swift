93bde62ce894a2c9dbda9fecaf15dbcae46596d3
juannluengo <juanluengogallego@gmail.com>
2024-09-15 21:27:01 +0200

    Change layout and incremation system

--- Points Watch App/PointsView.swift
+++ Points Watch App/PointsView.swift
@@ -3,17 +3,33 @@ import SwiftUI
 struct PointsView: View {
     var sport: String
     @EnvironmentObject var history: MatchHistory
+
+    // Default colors for players
+    let p1Color: Color = .blue
+    let p2Color: Color = .red
+
     @State private var pointsP1 = 0
     @State private var pointsP2 = 0
     @State private var setsP1 = 0
     @State private var setsP2 = 0
     @State private var actionHistory: [(pointsP1: Int, pointsP2: Int, setsP1: Int, setsP2: Int)] = []
-    
+
     @State private var animateP1 = false
     @State private var animateP2 = false
     @State private var animateFinish = false
-    
+
     @AppStorage("longPressEnabled") private var longPressEnabled = true
+
+    // The color of the top rectangle is based on who's winning. A tie is a mix of both
+    var topColor: Color {
+        if pointsP1 > pointsP2 {
+            return p1Color
+        } else if pointsP2 > pointsP1 {
+            return p2Color
+        } else {
+            return p1Color.blend(with: p2Color)
+        }
+    }
 
     var maxPoints: Int {
         switch sport {
@@ -26,77 +42,101 @@ struct PointsView: View {
 
     var body: some View {
         ScrollView {
-            VStack {
+            VStack(spacing: 10) {
+                // Top rectangle
                 Text("\(setsP1) : \(setsP2)")
-                    .font(.title2)
-                    .padding()
+                    .font(.largeTitle)
+                    .frame(maxWidth: .infinity, minHeight: 50)
+                    .background(topColor)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .padding([.leading, .trailing])
 
-                HStack {
+                // Separate rectangles
+                Spacer(minLength: 10)
+
+                // Player Rectangles
+                HStack(spacing: 10) {
+                    // Player 1
                     VStack {
-                        Text("P1: \(displayScore(points: pointsP1))")
-                            .font(.title2)
-
-                        Button(action: {
-                            if !longPressEnabled {
-                                incrementP1()
-                                provideHapticFeedback()
-                                animateButton(for: .p1)
-                            }
-                        }) {
-                            Text("+")
-                                .font(.largeTitle)
-                                .frame(width: 40, height: 40)
-                        }
-                        .scaleEffect(animateP1 ? 1.2 : 1.0)
-                        .animation(.easeInOut(duration: 0.1), value: animateP1)
-                        .simultaneousGesture(
-                            LongPressGesture(minimumDuration: 1.0)
-                                .onEnded { _ in
-                                    if longPressEnabled {
-                                        incrementP1()
-                                        provideHapticFeedback()
-                                        animateButton(for: .p1)
-                                    }
-                                }
-                        )
+                        Spacer()
+                        Text("\(displayScore(points: pointsP1))")
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                            .scaleEffect(animateP1 ? 1.2 : 1.0)
+                            .animation(.easeInOut(duration: 0.1), value: animateP1)
+                        Spacer()
                     }
-                    VStack {
-                        Text("P2: \(displayScore(points: pointsP2))")
-                            .font(.title2)
-
-                        Button(action: {
-                            if !longPressEnabled {
-                                incrementP2()
-                                provideHapticFeedback()
-                                animateButton(for: .p2)
-                            }
-                        }) {
-                            Text("+")
-                                .font(.largeTitle)
-                                .frame(width: 40, height: 40)
-                        }
-                        .scaleEffect(animateP2 ? 1.2 : 1.0)
-                        .animation(.easeInOut(duration: 0.1), value: animateP2)
-                        .simultaneousGesture(
-                            LongPressGesture(minimumDuration: 1.0)
-                                .onEnded { _ in
-                                    if longPressEnabled {
-                                        incrementP2()
-                                        provideHapticFeedback()
-                                        animateButton(for: .p2)
-                                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(p1Color)
+                    .cornerRadius(10)
+                    .gesture(
+                        DragGesture()
+                            .onEnded { value in
+                                if value.translation.height < 0 {
+                                    incrementP1()
+                                    provideHapticFeedback()
+                                    animateButton(for: .p1)
+                                } else if value.translation.height > 0 {
+                                    decrementP1()
+                                    provideHapticFeedback()
+                                    animateButton(for: .p1)
                                 }
-                        )
+                            }
+                    )
+                    .onTapGesture {
+                        incrementP1()
+                        provideHapticFeedback()
+                        animateButton(for: .p1)
+                    }
+
+                    // Player 2
+                    VStack {
+                        Spacer()
+                        Text("\(displayScore(points: pointsP2))")
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                            .scaleEffect(animateP2 ? 1.2 : 1.0)
+                            .animation(.easeInOut(duration: 0.1), value: animateP2)
+                        Spacer()
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(p2Color)
+                    .cornerRadius(10)
+                    .gesture(
+                        DragGesture()
+                            .onEnded { value in
+                                if value.translation.height < 0 {
+                                    incrementP2()
+                                    provideHapticFeedback()
+                                    animateButton(for: .p2)
+                                } else if value.translation.height > 0 {
+                                    decrementP2()
+                                    provideHapticFeedback()
+                                    animateButton(for: .p2)
+                                }
+                            }
+                    )
+                    .onTapGesture {
+                        incrementP2()
+                        provideHapticFeedback()
+                        animateButton(for: .p2)
                     }
                 }
-                .padding()
+                .padding([.leading, .trailing])
+                .frame(height: 300) // This is to make the players rectangles longer
 
+                // This is to push the buttons lower
+                Spacer()
+
+                // Undo Button
                 Button(action: redoLastAction) {
                     Text("Undo")
                         .foregroundColor(.orange)
                 }
                 .padding()
 
+                // Finish Button
                 Button(action: {
                     if !longPressEnabled {
                         resetMatch()
@@ -126,10 +166,12 @@ struct PointsView: View {
         // .navigationTitle("\(sport)")
     }
 
+    // Haptic feedback (que vibre cuando pinchas)
     private func provideHapticFeedback() {
         WKInterfaceDevice.current().play(.success)
     }
 
+    // Animate button (so that is slightly increase to show you that you have pressed it)
     private func animateButton(for button: ButtonType) {
         switch button {
         case .p1:
@@ -153,12 +195,6 @@ struct PointsView: View {
     private enum ButtonType {
         case p1, p2, finish
     }
-    
-
-    private enum Player {
-        case p1, p2
-    }
-
 
     // Score display function for Tennis, Padel, and other sports
     func displayScore(points: Int) -> String {
@@ -219,6 +255,22 @@ struct PointsView: View {
         }
     }
 
+    // Decrement logic for Player 1
+    func decrementP1() {
+        if pointsP1 > 0 {
+            saveCurrentState()
+            pointsP1 -= 1
+        }
+    }
+
+    // Decrement logic for Player 2
+    func decrementP2() {
+        if pointsP2 > 0 {
+            saveCurrentState()
+            pointsP2 -= 1
+        }
+    }
+
     // Tennis/Padel increment logic
     func incrementTennisPadel(player: Int) {
         if player == 1 {
@@ -254,7 +306,7 @@ struct PointsView: View {
 
     // Increment logic for Badminton, Squash, and Ping Pong
     func incrementBadmintonSquashPingPong(player: Int) {
-        let maxThreshold = sport == "Badminton" ? 20 : 10 // Deuce threshold for Badminton (20), Squash/Ping Pong (10)
+        let maxThreshold = sport == "Badminton" ? 20 : 10 // Deuce threshold
         if player == 1 {
             if pointsP1 >= maxThreshold && pointsP1 - pointsP2 >= 1 {
                 winGame(player: 1)
@@ -280,12 +332,12 @@ struct PointsView: View {
         resetPoints()
     }
 
-    // Save the current state (both points and sets) before taking an action
+    // Save the current state before taking an action
     func saveCurrentState() {
         actionHistory.append((pointsP1: pointsP1, pointsP2: pointsP2, setsP1: setsP1, setsP2: setsP2))
     }
 
-    // Redo last action (restore the previous state of points and sets)
+    // Undo last action
     func redoLastAction() {
         guard !actionHistory.isEmpty else { return }
         let lastState = actionHistory.removeLast()
@@ -309,5 +361,18 @@ struct PointsView: View {
         setsP1 = 0
         setsP2 = 0
         actionHistory.removeAll() // Clear history when resetting the match
+    }
+}
+
+// Extension to blend two colors
+extension Color {
+    func blend(with color: Color) -> Color {
+        let uiColor1 = UIColor(self)
+        let uiColor2 = UIColor(color)
+        var r1: CGFloat = 0; var g1: CGFloat = 0; var b1: CGFloat = 0; var a1: CGFloat = 0
+        var r2: CGFloat = 0; var g2: CGFloat = 0; var b2: CGFloat = 0; var a2: CGFloat = 0
+        uiColor1.getRed(&r1, green: &g1, blue: &b1, alpha: &a1)
+        uiColor2.getRed(&r2, green: &g2, blue: &b2, alpha: &a2)
+        return Color(red: (r1 + r2) / 2, green: (g1 + g2) / 2, blue: (b1 + b2) / 2)
     }
 }
